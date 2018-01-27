a350b5de36cfe4eddb5130f116c000bb781dda54
Eveline Debora Nuta <debbie.evelyn@gmail.com>
2018-01-27 16:25:46 +0100

    changes to application layout

--- src/main/java/at/ac/univie/FiWi/LiM/view/MainFrame.java
+++ src/main/java/at/ac/univie/FiWi/LiM/view/MainFrame.java
@@ -7,7 +7,7 @@ import java.awt.*;
 
 public class MainFrame extends JFrame {
 
-	public static final String BUTTON_CALCULATE = "Option Pricing";
+	public static final String BUTTON_CALCULATE = "Calculate Option Pricing";
 	public static final String RADIO_OPTIONTYPE_CALL = "Call Option";
 	public static final String RADIO_OPTIONTYPE_PUT = "Put Option";
 	public static final String RADIO_OPTIONSTYLE_EUROPEAN = "European";
@@ -40,7 +40,8 @@ public class MainFrame extends JFrame {
 		optionTypeRadioGroup.add(optionTypeCallRadio);
 		optionTypeRadioGroup.add(optionTypePutRadio);
 
-		JPanel optionTypePanel = new JPanel(new GridLayout(1,0));
+		JPanel optionTypePanel = new JPanel(new GridLayout(0,1));
+		optionTypePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		optionTypePanel.add(optionTypeCallRadio);
 		optionTypePanel.add(optionTypePutRadio);
 
@@ -56,35 +57,45 @@ public class MainFrame extends JFrame {
 		optionStyleGroup.add(optionStyleLookbackFloatingRadio);
 		optionStyleGroup.add(optionStyleLookbackFixedRadio);
 
-		JPanel optionStylePanel = new JPanel(new GridLayout(1,0));
+		JPanel optionStylePanel = new JPanel(new GridLayout(0,1));
+		optionStylePanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
 		optionStylePanel.add(optionStyleEuropeanRadio);
 		optionStylePanel.add(optionStyleAmericanRadio);
 		optionStylePanel.add(optionStyleLookbackFloatingRadio);
 		optionStylePanel.add(optionStyleLookbackFixedRadio);
 
-		JPanel radioButtonPanel = new JPanel(new BorderLayout());
+		JPanel radioButtonPanel = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		radioButtonPanel.setLayout(new BoxLayout(radioButtonPanel, BoxLayout.Y_AXIS));
 		radioButtonPanel.add(optionTypePanel, BorderLayout.NORTH);
-		radioButtonPanel.add(optionStylePanel, BorderLayout.SOUTH);
-		this.add(radioButtonPanel, BorderLayout.NORTH);
+		radioButtonPanel.add(optionStylePanel);
+		this.add(radioButtonPanel, BorderLayout.EAST);
 
 
 
 		//JPanel labelPanel = new JPanel();
-    JPanel dataPanel = new JPanel();
-    JPanel panel = new JPanel(new BorderLayout());
+    JPanel dataPanel = new JPanel(new GridLayout(0,2));
+    JPanel panel = new JPanel(new FlowLayout(FlowLayout.LEFT));
 
-		JButton button = new JButton(BUTTON_CALCULATE);
-		button.addActionListener (appController);
+	JButton button = new JButton(BUTTON_CALCULATE);
+	button.addActionListener (appController);
     InitNumberSpinners(dataPanel);
+	JLabel emptyLabel1 = new JLabel(" ", SwingConstants.LEFT);
+	dataPanel.add(emptyLabel1);
+	JLabel emptyLabel2 = new JLabel(" ", SwingConstants.LEFT);
+	dataPanel.add(emptyLabel2);
+	JLabel emptyLabel3 = new JLabel(" ", SwingConstants.LEFT);
+	dataPanel.add(emptyLabel3);
     dataPanel.add(button);
-
-		JLabel optionPricingLabel = new JLabel("Option Pricing:");
+	JLabel emptyLabel4 = new JLabel(" ", SwingConstants.LEFT);
+	dataPanel.add(emptyLabel4);
+	JLabel emptyLabel5 = new JLabel(" ", SwingConstants.LEFT);
+	dataPanel.add(emptyLabel5);
+		JLabel optionPricingLabel = new JLabel("Option Pricing result:", SwingConstants.LEFT);
 		optionPricingField = new JTextField();
 		dataPanel.add(optionPricingLabel);
 		dataPanel.add(optionPricingField);
 
     //labelPanel.setLayout(new BoxLayout(labelPanel, BoxLayout.Y_AXIS));
-    dataPanel.setLayout(new BoxLayout(dataPanel, BoxLayout.Y_AXIS));
     //panel.add(labelPanel, BorderLayout.WEST);
     panel.add(dataPanel,  BorderLayout.WEST);
     this.add(panel);
@@ -99,23 +110,17 @@ public class MainFrame extends JFrame {
   
   public void InitNumberSpinners(JPanel panel)
   {
-	  JLabel label1 = new JLabel();
-	  label1.setText("Initial Stock Value:");
+	  JLabel label1 = new JLabel("Initial Stock Value:", SwingConstants.LEFT);
 	  initialStockPriceSpinner = new JSpinner(new SpinnerNumberModel(100, 0, 10000000, 0.1));
-	  JLabel label2 = new JLabel();
-	  label2.setText("Strike Price:");
+	  JLabel label2 = new JLabel("Strike Price:", SwingConstants.LEFT);
 	  strikePriceSpinner = new JSpinner(new SpinnerNumberModel(100, 0, 10000000, 0.1));
-	  JLabel label3 = new JLabel();
-	  label3.setText("Maturity:");
+	  JLabel label3 = new JLabel("Maturity:", SwingConstants.LEFT);
 	  maturitySpinner = new JSpinner(new SpinnerNumberModel(1, 0, 10, 0.1));    
-	  JLabel label4 = new JLabel();
-	  label4.setText("Volatility:");
+	  JLabel label4 = new JLabel("Volatility:", SwingConstants.LEFT);
 	  dispersionSpinner = new JSpinner(new SpinnerNumberModel(0.2, 0, 10, 0.1));
-	  JLabel label6 = new JLabel();
-	  label6.setText("Number of Simulations:");
+	  JLabel label6 = new JLabel("Number of Simulations:", SwingConstants.LEFT);
 	  numOfSimulationsSpinner = new JSpinner(new SpinnerNumberModel(10000, 0, 100000000, 1));
-	  JLabel label7 = new JLabel();
-	  label7.setText("Risk-free rate:");   
+	  JLabel label7 = new JLabel("Risk-free rate:", SwingConstants.LEFT);
 	  riskFreeRateSpinner = new JSpinner(new SpinnerNumberModel(0.05, 0, 10, 0.1));  
 	  panel.add(label1);
 	  panel.add(initialStockPriceSpinner);
